af0b44fe30b2181243fa1e48b1cb5dd4e1a7c544
WSBTM95 <77251595+WSBTM95@users.noreply.github.com>
2022-02-08 14:14:34 -0600

    Update hwkThree.c

--- hwkThree.c
+++ hwkThree.c
@@ -1,8 +1,12 @@
 char hwkThree(unsigned int x) {
     char expo;
-    x = x << 1;
-    x = x & 267386880;
+    printf("%i ", x);
+//    x = x << 1;
+//printf("%i ", x);
+    x = x & 2139095040; //7F800000
+//printf("%i ", x);
     x = x >> 23;
+//printf("%i ", x);
     expo = x - 127;
     return expo;
 }
