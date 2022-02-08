67f37d34d0bd16b0c22b98f81fede09c6cc30f38
WSBTM95 <77251595+WSBTM95@users.noreply.github.com>
2022-02-08 13:57:36 -0600

    Update hwkThree.c

--- hwkThree.c
+++ hwkThree.c
@@ -1,42 +1,7 @@
 char hwkThree(unsigned int x) {
     char expo;
-    char string[33];
-    unsigned int i = 1;
-    unsigned int j = 0;
-    int buffer;
-    while (x > ((2 * i)-1))
-    {
-        i *= 2;
-    }
-    for (i; i > 0; i /= 2) {
-        buffer = x - i;
-        if (buffer >= 0) {
-            x -= i;
-            string[j] = '1';
-            j++;
-        }else if (buffer < 0) {
-            string[j] = '0';
-            j++;
-        }
-        if (j == 8) {
-            string[j] = '\0';
-            break;
-        }
-    }
-//printf("%s ", string);
-    buffer = 128;
-    j = 0;
-    for (i = 0; i < 8; i++) {
-        if (string[i] == '1') {
-            j += buffer;
-            buffer /= 2;
-        }else {
-            buffer /= 2;
-        }
-    }
-//printf("%i ", j);
-    j -= 127;
-//printf("%i ", j);
-    expo = j;
+    x = x << 1;
+    x = x & 0FF00000;
+    expo = x - 127;
     return expo;
 }
